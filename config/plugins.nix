f9d4ff0c5a6d0a50395be8253e051a00fc1fef3b
Benedikt von Blomberg <github@bvb.anonaddy.com>
2024-09-26 14:10:20 +0200

    lsp

--- config/plugins.nix
+++ config/plugins.nix
@@ -22,5 +22,33 @@
       settings.current_line_blame = true;
     };
 
+    # Language Servers
+    lsp = {
+      enable = true;
+      servers = {
+        nil-ls.enable = true; # Nix
+        pyright.enable = true; # Python
+        clangd.enable = true; # C/C++
+        bashls.enable = true; # Bash
+        dockerls.enable = true; # Docker
+        marksman.enable = true; # Markdown
+        yamlls.enable = true; # YAML
+
+        #ts-ls.enable = true; # TS/JS
+        #cssls.enable = true; # CSS
+        #html.enable = true; # HTML
+        #phpactor.enable = true; # PHP
+        #lua-ls = { # Lua
+        #  enable = true;
+        #  settings.telemetry.enable = false;
+        #};
+        ## Rust
+        #rust-analyzer = {
+        #  enable = true;
+        #  installRustc = true;
+        #  installCargo = true;
+        #};
+      };
+    };
   };
 }
