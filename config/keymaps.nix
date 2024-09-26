3557c252b8cf6c4da1ce8b375322e2c925d79a0a
Benedikt von Blomberg <github@bvb.anonaddy.com>
2024-09-26 14:06:18 +0200

    new keybindings for telescope

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -6,5 +6,17 @@
       action = "<cmd>Telescope find_files<CR>";
       key = "<leader>ff";
     }
+    {
+      action = "<cmd>Telescope oldfiles<CR>";
+      key = "<leader>fh";
+    }
+    {
+      action = "<cmd>Telescope buffers<CR>";
+      key = "<leader>fb";
+    }
+    {
+      action = "<cmd>Telescope live_grep<CR>";
+      key = "<leader>fg";
+    }
   ];
 }
