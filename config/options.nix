793dab28236ddce2eb3db97bb06aa7d4a2698f01
Benedikt von Blomberg <github@bvb.anonaddy.com>
2024-09-26 14:07:19 +0200

    better tabs, highlight current line

--- config/options.nix
+++ config/options.nix
@@ -2,7 +2,13 @@
   globalOpts = {
     number = true;
     relativenumber = true;
+    
+    cursorline = true;
 
+    tabstop = 2;
     shiftwidth = 2;
+    softtabstop = 0;
+    expandtab = true;
+    smarttab = true;
   };
 }
