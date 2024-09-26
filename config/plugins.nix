074707544a8d4b7b15e6e03d663575a57c12efe5
Benedikt von Blomberg <github@bvb.anonaddy.com>
2024-09-26 14:09:12 +0200

    new statusline + gitblame

--- config/plugins.nix
+++ config/plugins.nix
@@ -1,10 +1,21 @@
 {
   plugins = {
-    # bufferline.enable = false;
-    telescope.enable = true;
-    telescope.extensions.fzf-native.enable = true;
-    telescope.extensions.file-browser.enable = true;
+    telescope = {
+      enable = true;
+      extensions.fzf-native.enable = true;
+      extensions.file-browser.enable = true;
+    };
     treesitter.enable = true;
     web-devicons.enable = true;
+
+    # Statusline at the bottom
+    lualine.enable = true;
+
+    # Gitblame 
+    gitsigns = {
+      enable = true;
+      settings.current_line_blame = true;
+    };
+
   };
 }
