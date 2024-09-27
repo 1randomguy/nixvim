5b72272a83d76f9050f33a113366c4d125025a34
Benedikt von Blomberg <github@bvb.anonaddy.com>
2024-09-27 12:42:59 +0200

    add completions

--- config/plugins.nix
+++ config/plugins.nix
@@ -56,22 +56,55 @@
         dockerls.enable = true; # Docker
         marksman.enable = true; # Markdown
         yamlls.enable = true; # YAML
-
-        #ts-ls.enable = true; # TS/JS
-        #cssls.enable = true; # CSS
-        #html.enable = true; # HTML
-        #phpactor.enable = true; # PHP
-        #lua-ls = { # Lua
-        #  enable = true;
-        #  settings.telemetry.enable = false;
-        #};
-        ## Rust
-        #rust-analyzer = {
-        #  enable = true;
-        #  installRustc = true;
-        #  installCargo = true;
-        #};
       };
+    };
+
+    # Completions
+    cmp = {
+      enable = true;
+      settings = {
+        autoEnableSources = true;
+        experimental = { ghost_text = true; };
+        performance = {
+          debounce = 60;
+          fetchingTimeout = 200;
+          maxViewEntries = 30;
+        };
+        formatting = { fields = [ "abbr" "kind" "menu" ]; };
+        sources = [
+          { name = "nvim_lsp"; }
+          {
+            name = "buffer"; # text within current buffer
+            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+            keywordLength = 3;
+          }
+          # { name = "copilot"; } # enable/disable copilot
+          {
+            name = "path"; # file system paths
+            keywordLength = 3;
+          }
+        ];
+
+        window = {
+          completion = { border = "rounded"; };
+          documentation = { border = "rounded"; };
+        };
+
+        mapping = {
+          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          "<C-j>" = "cmp.mapping.select_next_item()";
+          "<C-k>" = "cmp.mapping.select_prev_item()";
+          "<C-e>" = "cmp.mapping.abort()";
+          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+        };
+      };
+    };
+    cmp-cmdline = {
+      enable = true; # autocomplete for cmdline
     };
   };
 }
