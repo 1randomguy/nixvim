f611bc60a1f7bb34d46311159d6c3ce52868fc57
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-02-02 21:12:06 +0100

    try luaconfig again

--- config/plugins.nix
+++ config/plugins.nix
@@ -72,6 +72,19 @@
 
     noice = {
       enable = true;
+      luaConfig.pre = ''
+      require("lualine").setup({
+        sections = {
+          lualine_x = {
+            {
+              require("noice").api.statusline.mode.get,
+              cond = require("noice").api.statusline.mode.has,
+              color = { fg = "#ff9e64" },
+            }
+          },
+        },
+      })
+      '';
       settings = {
         notify.enabled = true;
       };
