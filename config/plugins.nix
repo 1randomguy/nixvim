1524ec241959248f3bb43e18e577c4ba6e52617d
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-02-02 20:35:32 +0100

    add macro to noice

--- config/plugins.nix
+++ config/plugins.nix
@@ -70,7 +70,22 @@
     # Icons
     web-devicons.enable = true;
 
-    noice.enable = true;
+    noice = {
+      enable = true;
+      luaConfig.pre = ''
+      require("lualine").setup({
+        sections = {
+          lualine_x = {
+            {
+              require("noice").api.statusline.mode.get,
+              cond = require("noice").api.statusline.mode.has,
+              color = { fg = "#ff9e64" },
+            }
+          },
+        },
+      })
+      '';
+    };
 
     # Undotree
     undotree = {
