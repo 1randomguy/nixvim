8f68d35a342d3ca03df76da1dd62e84f9a41ea0b
Benedikt von Blomberg <github@bvb.anonaddy.com>
2025-04-29 13:37:28 +0200

    .

--- config/plugins.nix
+++ config/plugins.nix
@@ -210,6 +210,12 @@
         };
         latex = {
           enabled = true;
+          render_modes = false;
+          converter = "latex2text";
+          highlight = "RenderMarkdownMath";
+          position = "above";
+          top_pad = 0;
+          bottom_pad = 0;
         };
       };
     };
